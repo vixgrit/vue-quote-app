6480dd2c871655bf1f31aa67ebb8807e3cc9708e
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-05 00:13:57 -0400

    add progress bar displaying num of quotes added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="container">
-    
+  <div class="container">
+    <div class="row">
+      <progress-bar :quotesAdded="quotesAdded"></progress-bar>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+  import ProgressBar from './components/ProgressBar.vue'
+  export default {
+    components: {
+      'progress-bar': ProgressBar
+    },
+    data() {
+      return {
+        quotesAdded: 3
+      }
     }
+  }
 </script>
 
 <style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+  <div class="col-xs-12">
+    <h3>Quotes Added</h3>
+    <div class="progress-bar">
+      <div class="progress-status" :style="progressStatus">
+        <span v-if="quotesAdded > 0" class="progress-text">{{quotesAdded}} / 10</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['quotesAdded'],
+  data() {
+    return {
+      progressStatus: {
+        width: this.quotesAdded / 10 * 100 + '%'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .progress-bar {
+    border-radius: 15px;
+    height: 24px;
+    width: 100%;
+    background-color: #eee;
+  }
+
+  .progress-status {
+    border-radius: 15px;
+    width: 0px;
+    height: 24px;
+    background-color: #6ca6cd;
+  }
+
+  .progress-text {
+    font-size: 18px;
+  }
+</style>
