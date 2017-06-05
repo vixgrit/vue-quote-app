d43d67e66499b096836075973f6951635b8edb11
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-05 16:34:39 -0400

    fix alignment and add 'Add Quote' button

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
-<template>
+<template lang="html">
   <div class="container">
     <div class="row">
       <progress-bar :quotesAdded="quotesAdded"></progress-bar>
+    </div>
+    <div class="row">
+      <quote-input></quote-input>
     </div>
   </div>
 </template>
 
 <script>
   import ProgressBar from './components/ProgressBar.vue'
+  import QuoteInput from './components/QuoteInput.vue'
   export default {
     components: {
-      'progress-bar': ProgressBar
+      'progress-bar': ProgressBar,
+      'quote-input': QuoteInput
     },
     data() {
       return {
-        quotesAdded: 0
+        quotesAdded: 3
       }
     }
   }
---
+++ src/components/QuoteInput.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div class="col-xs-12">
+    <span>Quote</span>
+    <textarea name="name" rows="4"></textarea>
+    <button type="button" class="btn btn-primary">Add Quote</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css" scoped>
+  div {
+    margin-top: 30px;
+  }
+
+  span {
+    font-size: 14px;
+    font-weight: bold;
+    margin-left: 20%;
+  }
+
+  textarea {
+    width: 60%;
+    display: block;
+    margin: 0 auto;
+    resize: none;
+  }
+
+  button {
+    margin: 30px auto;
+    display: block;
+  }
+</style>
